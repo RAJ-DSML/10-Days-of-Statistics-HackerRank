991c248c824573d9448f93085d3d9a3c7904739a
RAJ KUMAR PAL <68045953+RAJ-DSML@users.noreply.github.com>
2021-10-29 23:38:24 +0530

    Day 2 file 2 added

---
+++ Day 2 - More Dice.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 29 23:36:27 2021
+
+@author: rajkumarpal
+Problem: Day 2 - More Dice
+"""
+
+
+Task
+In a single toss of 2 fair (evenly-weighted) six-sided dice, find the probability that the values rolled by each die will be different and the two dice have a sum of 6.
+
+Ans: 1/9
