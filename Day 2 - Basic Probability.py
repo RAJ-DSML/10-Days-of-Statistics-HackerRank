6026c6f28cb12c3cfb0e9e5384cb5158d35f4af9
RAJ KUMAR PAL <68045953+RAJ-DSML@users.noreply.github.com>
2021-10-29 23:34:25 +0530

    Day 2 file 1 added

---
+++ Day 2 - Basic Probability.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 29 23:31:28 2021
+
+@author: rajkumarpal
+Problem Name : Day 2 - Basic Probability
+"""
+
+Task
+In a single toss of  fair (evenly-weighted) six-sided dice, find the probability that their sum will be at most .
+
+Ans: 5/6
