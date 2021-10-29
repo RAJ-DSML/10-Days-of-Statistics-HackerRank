13c66d7a86b07466e565b836fc17138ebdd73f04
RAJ KUMAR PAL <68045953+RAJ-DSML@users.noreply.github.com>
2021-10-29 23:44:03 +0530

    Day 2 file 3 added

---
+++ Day 2 - Compound Event Probability.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 29 23:39:29 2021
+
+@author: rajkumarpal
+Problem: Day 2 - Compound Event Probability
+"""
+
+Task
+There are 3 urns labeled X, Y, and Z.
+
+
+Urn X contains 4 red balls and 3 black balls.
+Urn Y contains 5 red balls and 4 black balls.
+Urn Z contains 4 red balls and 4 black balls.
+
+One ball is drawn from each of the 3 urns. 
+What is the probability that, of the 3 balls drawn, 2 are red and 1 is black?
+
+Ans: 17/42
