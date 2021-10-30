a37a5e465956b40bb36d7f423222fc1e11775a87
RAJ KUMAR PAL <68045953+RAJ-DSML@users.noreply.github.com>
2021-10-30 08:31:17 +0530

    Day 3 file 2 added

---
+++ Day 3 - Cards of the same suit.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 30 08:28:13 2021
+
+@author: rajkumarpal
+Problem: Day 3 - Cards of the same suit
+"""
+
+
+Task
+You draw 2 cards from a standard 52-card deck without replacing them. 
+What is the probability that both cards are of the same suit?
+
+Ans: 12/51
