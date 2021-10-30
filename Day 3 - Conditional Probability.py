c28207fe7374708ee64a8bec3f7b9c49b64df8a5
RAJ KUMAR PAL <68045953+RAJ-DSML@users.noreply.github.com>
2021-10-30 08:27:49 +0530

    Day 3 file 1 added

---
+++ Day 3 - Conditional Probability.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 30 08:25:30 2021
+
+@author: rajkumarpal
+Problem: Day 3 - Conditional Probability
+"""
+
+Task
+Suppose a family has 2 children, one of which is a boy. What is the probability that both children are boys?
+
+Ans: 1/3
