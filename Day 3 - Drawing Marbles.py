ff0b71e45c58fa2f4dd2fea79f54b934164ad24b
RAJ KUMAR PAL <68045953+RAJ-DSML@users.noreply.github.com>
2021-10-30 08:42:47 +0530

    Day 3 file 3 added

---
+++ Day 3 - Drawing Marbles.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 30 08:31:58 2021
+
+@author: rajkumarpal
+Problem: Day 3 - Drawing Marbles
+"""
+
+
+Task
+A bag contains 3 red marbles and 4 blue marbles. 
+Then, 2 marbles are drawn from the bag, at random, without replacement. 
+If the first marble drawn is red, what is the probability that the second marble is blue?
+
+Ans: 2/3
