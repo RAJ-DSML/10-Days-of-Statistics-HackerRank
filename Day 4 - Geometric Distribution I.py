95eb03f5febdea36104be0c81db2d7890a85b2a8
RAJ KUMAR PAL <68045953+RAJ-DSML@users.noreply.github.com>
2021-11-02 00:02:03 +0530

    Day 4 file 3 added

--- Day 4 - Geometric Distribution I.py
+++ Day 4 - Geometric Distribution I.py
@@ -5,6 +5,10 @@ Created on Mon Nov  1 23:59:07 2021
 
 @author: rajkumarpal
 Problem: Day 4 - Geometric Distribution I
+
+---Task---
+The probability that a machine produces a defective product is 1/3. 
+What is the probability that the 1st defect occurs the 5th item produced?
 """
 
 
