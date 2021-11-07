1c024b4dc4258b1218646a498ce4630ea522297b
RAJ KUMAR PAL <68045953+RAJ-DSML@users.noreply.github.com>
2021-11-07 18:00:57 +0530

    Day 8 file 2 added

---
+++ Day 8 - Pearson Correlation Coefficient II.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov  7 17:57:34 2021
+
+@author: rajkumarpal
+Problem: Day 8 - Pearson Correlation Coefficient II
+
+Question: The regression line of y on x is 3x + 4y + 8 = 0, 
+and the regression line of x on y is 4x + 3y + 7 = 0. 
+What is the value of the Pearson correlation coefficient?
+
+"""
+
+Ans:-3/4
